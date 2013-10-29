7862483cd1167108d8a30ad108a01fd5bd1e577e
Sha Zhengju <handai.szj@taobao.com>
2013-10-29 14:36:54 +0800

    t4: two fixes for instance-ware loadavg
    1. instance's runq in /proc/loadavg is still global, change it to instance's internal number.
    2. Do not reset global counter 'avenrun'. Here it's intent to initialize cpuacct's avenrun,
    but no need since we already use kzalloc. The bug has effect on global loadavg while
    new cpuacct cgroup is created.
    
    
    Signed-off-by: Sha Zhengju <handai.szj@taobao.com>

--- fs/proc/loadavg.c
+++ fs/proc/loadavg.c
@@ -56,7 +56,7 @@ static int loadavg_proc_show(struct seq_file *m, void *v)
 		LOAD_INT(avnrun[0]), LOAD_FRAC(avnrun[0]),
 		LOAD_INT(avnrun[1]), LOAD_FRAC(avnrun[1]),
 		LOAD_INT(avnrun[2]), LOAD_FRAC(avnrun[2]),
-		nr_running(), nr_threads,
+		nr_runnable, nr_threads,
 		task_active_pid_ns(current)->last_pid);
 	return 0;
 }
