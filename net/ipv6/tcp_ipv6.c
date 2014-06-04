85f501bdf31e011305c9ef2d7f184788935b8289
Neal Cardwell <ncardwell@google.com>
2014-06-04 11:17:29 +0800

    tcp: extract code to compute SYNACK RTT
    In preparation for adding another spot where we compute the SYNACK
    RTT, extract this code so that it can be shared.
    
    [Note from Zheng Liu]
    We should backport this patch before 13th patch but we don't do that
    because it is only a minor change.  However, we must backport it now
    due to the next patch needs it. :(
    
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Zheng Liu <wenqing.lz@taobao.com>

--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -1447,9 +1447,7 @@ struct sock *tcp_v6_syn_recv_sock(struct sock *sk, struct sk_buff *skb,
 	tcp_sync_mss(newsk, dst_mtu(dst));
 	newtp->advmss = dst_metric(dst, RTAX_ADVMSS);
 	tcp_initialize_rcv_mss(newsk);
-	if (tcp_rsk(req)->snt_synack)
-		tcp_valid_rtt_meas(newsk,
-		    tcp_time_stamp - tcp_rsk(req)->snt_synack);
+	tcp_synack_rtt_meas(newsk, req);
 	newtp->total_retrans = req->retrans;
 
 	newinet->daddr = newinet->saddr = newinet->rcv_saddr = LOOPBACK4_IPV6;
