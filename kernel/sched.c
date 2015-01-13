9f02889205b30aeac6bec10e5b2d2de7c6f526dd
Peter Zijlstra <peterz@infradead.org>
2015-01-13 18:46:25 +0800

    [PATCH] sched, cgroup: Use exit hook to avoid use-after-free crash
    upstream commit 068c5cc5ac7414a8e9eb7856b4bf3cc4d4744267
    upstream commit d41d5a01631af821d3a3447e6613a316f5ee6c25
    
    By not notifying the controller of the on-exit move back to
    init_css_set, we fail to move the task out of the previous
    cgroup's cfs_rq. This leads to an opportunity for a
    cgroup-destroy to come in and free the cgroup (there are no
    active tasks left in it after all) to which the not-quite dead
    task is still enqueued.
    
    Reported-by: Miklos Vajna <vmiklos@frugalware.org>
    Fixed-by: Mike Galbraith <efault@gmx.de>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: <stable@kernel.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Charles Wang <muming.wq@taobao.com>

--- kernel/sched.c
+++ kernel/sched.c
@@ -11900,6 +11900,21 @@ cpu_cgroup_attach_task(struct cgroup *cgrp, struct task_struct *tsk)
 	sched_move_task(tsk);
 }
 
+static void
+cpu_cgroup_exit(struct cgroup_subsys *ss, struct cgroup *cgrp,
+		struct cgroup *old_cgrp, struct task_struct *task)
+{
+	/*
+	 * cgroup_exit() is called in the copy_process() failure path.
+	 * Ignore this case since the task hasn't ran yet, this avoids
+	 * trying to poke a half freed task state from generic code.
+	 */
+	if (!(task->flags & PF_EXITING))
+		return;
+
+	sched_move_task(task);
+}
+
 #ifdef CONFIG_FAIR_GROUP_SCHED
 static int cpu_shares_write_u64(struct cgroup *cgrp, struct cftype *cftype,
 				u64 shareval)
@@ -12220,6 +12235,7 @@ struct cgroup_subsys cpu_cgroup_subsys = {
 	.destroy	= cpu_cgroup_destroy,
 	.can_attach_task = cpu_cgroup_can_attach_task,
 	.attach_task	= cpu_cgroup_attach_task,
+	.exit           = cpu_cgroup_exit,
 	.populate	= cpu_cgroup_populate,
 	.subsys_id	= cpu_cgroup_subsys_id,
 	.early_init	= 1,
